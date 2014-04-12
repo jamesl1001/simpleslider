ce84cab8539cbc8311ef67687bbb1829aea79765
jamesl1001 <jamesl1001@msn.com>
2014-04-12 14:37:50 +0100

    Ensure images fill the wrapper regardless of orientation

--- simpleslider.js
+++ simpleslider.js
@@ -18,18 +18,7 @@ function simpleslider(ssR, ssF, ssD, ssP) {
         ssDirectory     = ssD,
         ssPrefix        = ssP,
         ssCurrentFrame  = 0,
-        ssDotsWidth     = 0;
-
-    // Create img elements if they don't already exist on the DOM
-    if(ssImages.length == 0) {
-        for(var i = 1; i <= ssFrames; i++) {
-            var ssImg = document.createElement('img');
-            ssImg.src = ssD + '/' + ssP + i + '.jpg';    // 'img/directory/prefix1.jpg'
-            ssWrapper.innerHTML += ssImg.outerHTML;
-        }
-    }
-
-    ssDotsWidth = ssFrames * 15;
+        ssDotsWidth     = ssFrames * 15;
 
     // Calculate aspect ratio
     var ssRatioSplit      = ssRatio.split(':');
@@ -38,6 +27,10 @@ function simpleslider(ssR, ssF, ssD, ssP) {
     // Set dimensions
     ss.style.paddingBottom = ssWrapper.style.paddingBottom = ssRatioPercentage + '%';
     ssDots.style.width     = ssDotsWidth + 'px';
+
+    // Get pixel dimensions
+    var ssWidth  = ssWrapper.offsetWidth;
+    var ssHeight = ssWrapper.offsetHeight;
 
     // Generate navigation dots
     for(var i = 0; i < ssFrames; i++) {
@@ -48,16 +41,47 @@ function simpleslider(ssR, ssF, ssD, ssP) {
 
     ssAllDots = ssDots.getElementsByTagName('div');
 
-    // Add current class to frame
+    // Create img elements if they don't already exist on the DOM
+    if(ssImages.length == 0) {
+        for(var i = 1; i <= ssFrames; i++) {
+            var ssImg = new Image();
+            ssImg.src = ssD + '/' + ssP + i + '.jpg'; // 'img/directory/prefix1.jpg'
+            ssWrapper.innerHTML += ssImg.outerHTML;
+        }
+        ssImages = ssWrapper.getElementsByTagName('img');
+    }
+
+    for(var i = 0, l = ssImages.length; i < l; i++) {
+        if(window.addEventListener) {
+            ssImages[i].addEventListener('load', function() { ssImgLoaded(this) });
+        } else if(window.attachEvent) {
+            ssImages[i].attachEvent('onload', function() { ssImgLoaded(this) });
+        }
+    }
+
+    function ssImgLoaded(imgElem) {
+        var img = new Image();
+        img.src = imgElem.src;
+
+        if(img.width < ssWidth) {
+            imgElem.className += ' full-width';
+        } else if(img.height < ssHeight) {
+            imgElem.className += ' full-height';
+        } else {
+            imgElem.className += ' full-width';
+        }
+    }
+
+    // Add current class to first frame
     function addCurrent(n) {
-        ssImages[n].className = 'current';
+        ssImages[n].className += ' current';
         ssAllDots[n].className += ' current';
     }
 
     // Clear all current classes
     function clearCurrent() {
         for(var i = 0; i < ssFrames; i++) {
-            ssImages[i].className = '';
+            ssImages[i].className = ssImages[i].className.replace(/ current/, '');
             ssAllDots[i].className = ssAllDots[i].className.replace(/ current/, '');
         }
     }
